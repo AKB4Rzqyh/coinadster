c7aba7f452733c5f89a6e4833e02e63aac2dc568
Akbarezqy <akbarezqyy@gmail.com>
2024-11-14 19:43:26 +0700

    Add files via upload

---
+++ cfg.php
@@ -0,0 +1,10 @@
+<?php
+
+//Isi USER-AGENT Sesuai Data Kalian
+$user = 'Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Mobile Safari/537.36';
+
+//Isi Cookie Sesuai Data Kalian
+$cookie = '_ga=GA1.1.2084945280.1730456580; bitmedia_fid=eyJmaWQiOiI5ZDRhYjJiOGU1ZGIxN2YzY2Y1MzFhMWEwMmU2NDA0NiIsImZpZG5vdWEiOiIxYTM2YTljM2UwNjYzM2QwZTNkYTVlNzM1ZTVlOTYwZiJ9; _fbp=fb.1.1730456582281.502451062243366100; _gcl_au=1.1.1398645193.1730456582.1599663431.1731070952.1731070951; SesHashKey=kx81nhqbvuiee39w; SesToken=ses_id=299889&ses_key=kx81nhqbvuiee39w; AccExist=299889; PHPSESSID=3991f9e830dadc00e39175726f7b399b; cf_clearance=4Bu9lHvrmBZk8KZ1.Qd5_OnLSYlmtGlgzgpH1hfPhIA-1731110497-1.2.1.1-8BApnKnmwFmLMjhHqW0_4WZyCWubm8JdkMYRE5.7S5h2grfKJlUx.m8BOl8yO0pUGyJ6WcB_MmveqUCwsunnPZLlfp.DOdlBNYiFxp8cofvNuZ_05QFGb38MP7bp8hQr7yh_dT7FKfz9W5tQWfRsZ0FPs8vTqiBrvyGm9SFC0OFqltvDSGCXCfoS5OcX7hEzV3AB2SavmOLWHateB3TOptIg0PWTDLRlZ6cU3C6ztoAygsMdAedqa.pG8H5FTC_dUQYTD3SkMiEkX1Lgma3AFyT9eIi6Nknxe3dmieUS5JSE9QUgUfTDIbcRWr9bhj2ti8kJQivhjKkbw_jvLZIaSYnll2Xl1Z3B93rY3sV.N97cxKWgKC4DABxarh.bRbEg_mxwT8TGnUWxE5ze.MxP6A; _ga_2D3162VN6Y=GS1.1.1731110497.22.1.1731110550.0.0.0';
+
+//Isi Url-Solvemedia Sesuai Data Kalian
+$url_solvemedia = 'https://api-secure.solvemedia.com/papi/_challenge.js?k=P7UUEs9AZZuZ1GtxoHnRsTutaLGvyb26;f=_ACPuzzleUtil.callbacks[0];l=en;t=img;s=standard;c=js,h5c,h5ct,svg,h5v,v/h264,v/webm,h5a,a/mp3,a/ogg,ua/chrome,ua/chrome95,os/android,os/android11,fwv/Bc1pKg.cobr30,jslib/jquery,htmlplus;am=CRkUQRYNhKAM1H93Fg2EoA;ca=ajax;ts=1639451983;ct=1639452499;th=white;r=0.35705062844554813';
